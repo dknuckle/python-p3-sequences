bd523c3063937d3ffb11ddb1cc91d0a994e27cbc
Dalton Knuckles <djck244@gmail.com>
2023-07-15 17:52:25 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    f_s = [0, 1]
+    
+    while len(f_s) < length:
+        next_number = f_s[-1] + f_s[-2]
+        f_s.append(next_number)
+    print(f_s)
+    return
